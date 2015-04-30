e31bf57e3b140ea3fa04311400ba6f93ce0381a9
arkady <larytet@yahoo.com>
2015-04-30 14:19:21 +0300

    Timers

--- src/Timers.h
+++ src/Timers.h
@@ -2,10 +2,19 @@
 
 typedef uint32_t TimerID;
 
+enum TimerError {
+      Ok           ,
+      Expired      ,
+      Stppped      ,
+      Illegal      ,
+      NoFreeTimer  ,
+      ILLEGAL_ID   ,
+};
+
 class Timer {
 public:
 
-    Timer() {
+    Timer() : isRunning(false) {
     }
 
 protected:
@@ -14,6 +23,10 @@ protected:
 
     void setId(TimerID id) {
         this->id = id;
+    }
+
+    void setIsrunning(bool flag) {
+        isRunning = flag;
     }
 
     void setApplicationData(uintptr_t applicationData) {
@@ -26,7 +39,7 @@ protected:
 
     TimerID id;
     uintptr_t applicationData;
-    unsigned int timerHandle;
+    bool isRunning;
 };
 
 typedef void (*TimerExpirationHandler)(void *);
@@ -40,29 +53,59 @@ class TimerListBase {
 
     }
 
+
 protected:
     uint32_t timeout;
     TimerExpirationHandler expirationHandler;
     bool callExpiredForStoppedTimers;
+
 };
 
 /**
  * A timer list is a queue of the started timers with the SAME timeout.
  * For example list of 1 s timers
  *
- * @param Size - maximum number of pending timers. Performance of the list does not
+ * @param Size maximum number of pending timers. Performance of the list does not
+ * @param Lock is a class synchronizing access to the list API
  * depend on the number of timers.
  */
-template<std::size_t Size> class TimerList : public TimerListBase {
+template<std::size_t Size, Lock> class TimerList : public TimerListBase {
 
     TimerList(uint32_t timeout, TimerExpirationHandler expirationHandler,
             bool callExpiredForStoppedTimers = false) : TimerListBase(timeout, expirationHandler, callExpiredForStoppedTimers) {
 
     }
 
+    /**
+     * Move a timer from list of of free timers and
+     * add the timer to the tail of the list of the started timers
+     *
+     * @param timer - will be set to reference the started timer
+     * @result TimerError:Ok if success
+     */
+    inline enum TimerError startTimer(Timer& timer) {
+        Lock();
+        if (!freeTimers.isEmpty) {
+            freeTimers.remove(timer);
+            runningTimers.add(timer);
+            return TimerError::Ok;
+        }
+        else {
+            return TimerError::NoFreeTimer;
+        }
+    }
+
+    inline enum TimerError stopTimer(Timer& timer) {
+        timer.setIsrunning(false);
+        return TimerError::Ok;
+    }
+
 protected:
 
+    friend class TimerSet;
+
     CyclicBuffer<Timer*, LockDummy, Size> runningTimers;
+    CyclicBuffer<Timer*, LockDummy, Size> freeTimers;
 };
 
 
