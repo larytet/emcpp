138f7f1dff2975c5ea938a74614306be4f9835a2
arkady <larytet@yahoo.com>
2015-05-02 14:45:53 +0300

    Template class fror hardware registers

--- src/Hardware.h
+++ src/Hardware.h
@@ -1,40 +1,52 @@
 #pragma once
 
-
 class HardwareModule {
 protected:
-    HardwareModule() {}
-    ~HardwareModule() {}
+    HardwareModule() {
+    }
+    ~HardwareModule() {
+    }
 };
 
-class HardwareRegister32 {
+template<typename IntegerType> class HardwareRegister {
 protected:
-    volatile uint32_t value;
-    inline uint32_t get() const {return value;}
-    inline void set(uint32_t value) {this->value = value;}
+    volatile IntegerType value;
+    static_assert(std::numeric_limits<IntegerType>::is_integer,
+            "HardwareRegister works only with integer types");
 };
-static_assert((sizeof(HardwareRegister32) == sizeof(uint32_t)), "HardwareRegister32 is not 32 bits");
 
+class HardwareRegister32: HardwareRegister<uint32_t> {
+protected:
+    inline uint32_t get() const {
+        return value;
+    }
+    inline void set(uint32_t value) {
+        this->value = value;
+    }
+};
+static_assert((sizeof(HardwareRegister32) == sizeof(uint32_t)),
+        "HardwareRegister32 is not 32 bits");
 
-
-class HardwareRegister32RO : public HardwareRegister32{
+class HardwareRegister32RO: public HardwareRegister32 {
 public:
     uint32_t operator=(const HardwareRegister32RO& r) const {
         return get();
     }
 };
-static_assert((sizeof(HardwareRegister32RO) == sizeof(uint32_t)), "HardwareRegister32RO is not 32 bits");
+static_assert((sizeof(HardwareRegister32RO) == sizeof(uint32_t)),
+        "HardwareRegister32RO is not 32 bits");
 
-class HardwareRegister32WO : public HardwareRegister32{
+class HardwareRegister32WO: public HardwareRegister32 {
 public:
     uint32_t operator=(uint32_t value) {
         set(value);
         return value;
     }
 };
-static_assert((sizeof(HardwareRegister32WO) == sizeof(uint32_t)), "HardwareRegister32WO is not 32 bits");
+static_assert((sizeof(HardwareRegister32WO) == sizeof(uint32_t)),
+        "HardwareRegister32WO is not 32 bits");
 
-class HardwareRegister32RW : public HardwareRegister32 {
+class HardwareRegister32RW: public HardwareRegister32 {
 public:
     uint32_t operator=(const HardwareRegister32RO& r) const {
         return get();
@@ -44,13 +56,16 @@ public:
         return value;
     }
 };
-static_assert((sizeof(HardwareRegister32RW) == sizeof(uint32_t)), "HardwareRegister32RW is not 32 bits");
+static_assert((sizeof(HardwareRegister32RW) == sizeof(uint32_t)),
+        "HardwareRegister32RW is not 32 bits");
 
-class HardwareRegister32NotUsed : HardwareRegister32 {
+class HardwareRegister32NotUsed: HardwareRegister32 {
 public:
-    HardwareRegister32NotUsed() {}
-    ~HardwareRegister32NotUsed() {}
+    HardwareRegister32NotUsed() {
+    }
+    ~HardwareRegister32NotUsed() {
+    }
 };
-static_assert((sizeof(HardwareRegister32NotUsed) == sizeof(uint32_t)), "HardwareRegister32NotUsed is not 32 bits");
-
+static_assert((sizeof(HardwareRegister32NotUsed) == sizeof(uint32_t)),
+        "HardwareRegister32NotUsed is not 32 bits");
 
