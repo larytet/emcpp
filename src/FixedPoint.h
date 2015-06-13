eff474a4bb9f2c3ed4acc853b78713bc55b764f5
arkady <larytet@yahoo.com>
2015-06-13 14:27:53 +0300

    Fixed point

--- src/FixedPoint.h
+++ src/FixedPoint.h
@@ -1,19 +1,20 @@
 #pragma once
 
-template <typename IntType> class D
+template <typename IntType> class FixedPoint
 {
-    int rep_;
+    IntType rep_;
     public:
-    D(int i=0) : rep_(i) {/*empty*/}
+    FixedPoint(IntType i=0) : rep_(i) {}
 
-    IntType& operator+=(IntType const& rhs)
+    IntType& operator+=(IntType const &rhs)
     { rep_ += rhs.rep_; return *this; }
-    bool operator<(IntType const& rhs) const
+
+    bool operator<(IntType const &rhs) const
     { return rep_ < rhs.rep_; }
 
-    IntType operator+(IntType const& lhs, IntType const& rhs)
-    { D tmp = lhs; tmp += rhs; return tmp; }
+    IntType operator+(IntType const &lhs, IntType const &rhs)
+    { FixedPoint tmp = lhs; tmp += rhs; return tmp; }
 
-    bool operator>=(IntType const& lhs, IntType const& rhs)
+    bool operator>=(IntType const &lhs, IntType const &rhs)
     { return !(lhs < rhs); }
 };
