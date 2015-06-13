4bed30f37b96b89987a4f4828ec247ffb475fed2
arkady <larytet@yahoo.com>
2015-06-13 15:50:47 +0300

    Fixed point

--- src/FixedPoint.h
+++ src/FixedPoint.h
@@ -5,25 +5,25 @@ class FixedPoint
 {
 public:
     typedef FixedPoint<IntType, Precision> T;
-    FixedPoint(double d) : v(static_cast<IntType>(d * FACTOR)) { }
+    FixedPoint(double d) : v(static_cast<IntType>(d * FACTOR)) {}
     FixedPoint(const T &rhs) : v(rhs.v) { }
-    T& operator+=(const T &rhs) { v += rhs.v; return *this; }
-    T& operator-=(const T &rhs) { v -= rhs.v; return *this; }
-    T& operator*=(const T &rhs) { v *= rhs.v; v >>= Precision; return *this; }
-    T& operator/=(const T &rhs) { v /= rhs.v; v *= FACTOR; return *this; }
-    T& operator=(const T &rhs) { v = rhs.v;return *this; }
-    double toDouble( ) const { return double(v) / FACTOR; }
+    T& operator+=(const T &rhs) {v += rhs.v; return *this;}
+    T& operator-=(const T &rhs) {v -= rhs.v; return *this;}
+    T& operator*=(const T &rhs) {v *= rhs.v; v >>= Precision; return *this;}
+    T& operator/=(const T &rhs) {v /= rhs.v; v *= FACTOR; return *this;}
+    T& operator=(const T &rhs) {v = rhs.v;return *this;}
+    double toDouble( ) const { return double(v) / FACTOR;}
 
-    friend T operator+(T lhs, const T &rhs) { return lhs += rhs; }
-    friend T operator-(T lhs, const T &rhs) { return lhs -= rhs; }
-    friend T operator*(T lhs, const T &rhs) { return lhs *= rhs; }
-    friend T operator/(T lhs, const T &rhs) { return lhs /= rhs; }
-    friend bool operator==(const T &lhs, const T &rhs) { return lhs.v == rhs.v; }
-    friend bool operator!=(const T &lhs, const T &rhs) { return lhs.v != rhs.v; }
-    friend bool operator>(const T &lhs, const T &rhs) { return lhs.v > rhs.v; }
-    friend bool operator<(const T &lhs, const T &rhs) { return lhs.v < rhs.v; }
-    friend bool operator>=(const T &lhs, const T &rhs) { return lhs.v >= rhs.v; }
-    friend bool operator<=(const T &lhs, const T &rhs) { return lhs.v <= rhs.v; }
+    friend T operator+(T lhs, const T &rhs) {return lhs += rhs;}
+    friend T operator-(T lhs, const T &rhs) {return lhs -= rhs;}
+    friend T operator*(T lhs, const T &rhs) {return lhs *= rhs;}
+    friend T operator/(T lhs, const T &rhs) {return lhs /= rhs;}
+    friend bool operator==(const T &lhs, const T &rhs) {return lhs.v == rhs.v;}
+    friend bool operator!=(const T &lhs, const T &rhs) {return lhs.v != rhs.v;}
+    friend bool operator>(const T &lhs, const T &rhs) {return lhs.v > rhs.v;}
+    friend bool operator<(const T &lhs, const T &rhs) {return lhs.v < rhs.v;}
+    friend bool operator>=(const T &lhs, const T &rhs) {return lhs.v >= rhs.v;}
+    friend bool operator<=(const T &lhs, const T &rhs) {return lhs.v <= rhs.v;}
 protected:
     const int FACTOR = 1 << Precision;
     int v;
