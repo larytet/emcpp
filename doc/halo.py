de24b0435e54c4414faa334ccf7d01866abafaa3
arkady <larytet@yahoo.com>
2015-10-22 15:39:56 +0300

    hola update

--- doc/halo.py
+++ doc/halo.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 '''
 Implement function check (text) which checks whether brackets within text are
 correctly nested. You need to consider brackets of three kinds: (), [], {}.
@@ -13,30 +16,53 @@ check("z([{}-()]{a})") -> true
 check("") -> true
 '''
 
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
+def isMatch(stack, ch):
+    if (ch == '}'):
+        if (len(stack) == 0):
+            print "12"
+            return False
+        stackTop = stack[-1]
+        if (stackTop != '{'):
+            return False
+    elif (ch == ')'):
+        if (len(stack) == 0):
+            return False
+        stackTop = stack[-1]
+        if (stackTop != '('):
+            return False
+    elif (ch == ']'):
+        if (len(stack) == 0):
+            return False
+        stackTop = stack[-1]
+        if (stackTop != '['):
+            return False
+    else:
+        return None
+    return True
+    
+    
 def checkParentheses(s):
     stack = []
-    openningParentheses = ['{', '[', '(']
-    closingParentheses = ['}', ']', ')']
-    pairs = {'}':'{', ']':'[', ')':'('}
+    print "s", s
     for ch in s:
-        if ch in openningParentheses:
+        print "ch=", ch
+        if ((ch == '{') or (ch == '[') or (ch == '(')):
+            print "append"
             stack.append(ch)
-        elif (ch in closingParentheses):
-            if (len(stack) == 0):
-                return False
-            translatedCh = pairs[ch]
-            stackTop = stack[-1]
-            if (stackTop == translatedCh):
-                stack.pop()
-            else:
+        else:
+            res = isMatch(stack, ch)
+            if (res == True):
+                if (len(stack) > 0):
+                    stack.pop()
+            elif (res == False):
                 return False
 
+    print "len(stack)", len(stack)
     return (len(stack) == 0)
 
 
-testLines =  ['1{2}3', '1(2)3', '1[2]3', '1[2(3)4]5', '1{2(3}4)5', "1}2{3", "1{"]
+testLines =  ['1{2}3']
+#, '1(2)3', '1[2]3', '1[2(3)4]5', '1{2(3}4)5', "1}2{3", "1{"]
 for line in testLines:
     print line, ":", checkParentheses(line)
 
