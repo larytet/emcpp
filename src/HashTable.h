eb694bbc07ac59a5477488aa4618ba65fc6c2fab
larytet <larytet@yahoo.com>
2016-08-08 14:48:53 +0300

    Add hash table

--- src/HashTable.h
+++ src/HashTable.h
@@ -1,6 +1,29 @@
 #pragma once
 
 
+/**
+ * Bob Jenkins hash function
+ * http://burtleburtle.net/bob/hash/doobs.html
+ */
+static inline uint_fast32_t one_at_a_time(uint8_t *key, uint_fast32_t len, uint_fast32_t seed = 0)
+{
+	uint_fast32_t   hash, i;
+
+	for (uint_fast32_t hash=0, i=0; i<len; ++i)
+	{
+		hash += key[i];
+		hash += (hash << 10);
+		hash ^= (hash >> 6);
+	}
+	hash += (hash << 3);
+	hash ^= (hash >> 11);
+	hash += (hash << 15);
+	return hash;
+}
+
+/**
+ * A simple hashable object for testing purposes
+ */
 template<typename Data, typename Key> class HashObject
 {
 public:
@@ -17,25 +40,6 @@ public:
 		return 0;
 	}
 
-	/**
-	 * Bob Jenkins hash function
-	 * http://burtleburtle.net/bob/hash/doobs.html
-	 */
-	uint_fast32_t one_at_a_time(uint8_t *key, uint_fast32_t len, uint_fast32_t seed = 0)
-	{
-		uint_fast32_t   hash, i;
-
-		for (uint_fast32_t hash=0, i=0; i<len; ++i)
-		{
-			hash += key[i];
-			hash += (hash << 10);
-			hash ^= (hash >> 6);
-		}
-		hash += (hash << 3);
-		hash ^= (hash >> 11);
-		hash += (hash << 15);
-		return hash;
-	}
 };
 
 template<typename Object, typename Key, typename Lock> class HashTable
